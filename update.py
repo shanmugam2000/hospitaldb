960cd7e829d2bc112555f8bfe37e56b9d652a449
shanmugam2000 <100746941+shanmugam2000@users.noreply.github.com>
2022-03-09 08:05:49 +0530

    update

---
+++ update.py
@@ -0,0 +1,23 @@
+import sqlite3
+data = sqlite3.connect("hospital.db")
+getId = input("Enter patient Id to be updated: ")
+getPatName = input("Enter new patient Name: ")
+getPatAddress = input("Enter new patient address: ")
+getPatPhno = input("Enter new patient phone number: ")
+getPatEmail = input("Enter new patient email: ")
+getPatPincode = input("Enter new patient pincode: ")
+data.execute("update patient set patgitname='"+getPatName+"',pataddress='"+getPatAddress+"',patphno="+getPatPhno+",\
+             patemail='"+getPatEmail+"',patpincode="+getPatPincode+" where id="+getId)
+
+data.commit()
+print("updated succesfully")
+result = data.execute("select * from patient")
+for i in result:
+      print("patient id", i[0])
+      print("patient name", i[2])
+      print("patient address", i[3])
+      print("patient phno", i[5])
+      print("patient email", i[4])
+      print("patient pincode", i[5])
+
+
